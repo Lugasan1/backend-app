1b038036ed202521119b3a79952ff28e82813b8e
Vinicius Gasparello <vinygasparello@gmail.com>
2024-05-29 18:30:43 -0300

    chore: add complete app

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //Port
+  PORT: process.env.PORT || 5000,
+
+  //Secret key for API
+  SECRET_KEY: "D-lRnkhY#",
+
+  //Gmail credentials for send email
+  EMAIL: "contato@itlbet.com.br",
+  PASSWORD: "jfvh wwjb bilv ntsw",
+
+  //Secret key for jwt
+  JWT_SECRET: "D-lRnkhY#",
+
+  //baseURL
+  baseURL: "https://service.livepay.app.br/",
+
+  //Firebase server key for send notification
+  SERVER_KEY: "AIzaSyCSFvenY7LWz5qTGIYB6Zfomx2aE9GLiyw",
+};
